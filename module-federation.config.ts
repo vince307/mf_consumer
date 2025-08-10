309c41fa18e648be8ccf0b94cffe7f7e703afe61
vince307 <kamil.piecuch@mycit.ie>
2025-08-10 14:41:05 +0200

    add MF retry option

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -1,19 +1,19 @@
-import { createModuleFederationConfig } from '@module-federation/rsbuild-plugin';
-import path from 'node:path';
+import { createModuleFederationConfig } from "@module-federation/rsbuild-plugin";
+import path from "node:path";
 
 export default createModuleFederationConfig({
-  name: 'mf_consumer',
-  remotes: {
-    // 'provider': 'rslib_provider@https://unpkg.com/module-federation-rslib-provider@latest/dist/mf/mf-manifest.json',
-  'provider': 'rslib_provider@https://mf-provider.vercel.app/mf-manifest.json',
-  },
-  shareStrategy: 'loaded-first',
-  shared: {
-    react: { singleton: true },
-    'react-dom': { singleton: true },
-  },
-  runtimePlugins: [
-    // path.join(__dirname, './src/runtime-plugin/retry.ts'),
-    path.join(__dirname, './src/runtime-plugin/fallback.ts'),
-  ],
+	name: "mf_consumer",
+	remotes: {
+		// 'provider': 'rslib_provider@https://unpkg.com/module-federation-rslib-provider@latest/dist/mf/mf-manifest.json',
+		provider: "rslib_provider@http://localhost:3001/mf-manifest.json",
+	},
+	shareStrategy: "loaded-first",
+	shared: {
+		react: { singleton: true },
+		"react-dom": { singleton: true },
+	},
+	runtimePlugins: [
+		path.join(__dirname, "./src/runtime-plugin/retry.ts"),
+		path.join(__dirname, "./src/runtime-plugin/fallback.ts"),
+	],
 });
